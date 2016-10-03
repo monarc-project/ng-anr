8f11a79415a7f87c5ee3c9a968522f77c11f25f3
Guillaume Lesniak <guillaume@netlor.fr>
2016-10-03 11:02:54 +0200

    Import most KB items that are editable in FO

---
+++ src/AmvService.js
@@ -0,0 +1,63 @@
+(function () {
+
+    angular
+        .module('BackofficeApp')
+        .factory('AmvService', [ '$resource', 'MassDeleteService', AmvService ]);
+
+    function AmvService($resource, MassDeleteService) {
+        var self = this;
+
+        self.AmvResource = $resource('/api/amvs/:amvId', { amvId: '@id' },
+            {
+                'update': {
+                    method: 'PUT'
+                },
+                'patch': {
+                    method: 'PATCH'
+                },
+                'query': {
+                    isArray: false
+                }
+            });
+
+        var getAmvs = function (params) {
+            return self.AmvResource.query(params).$promise;
+        };
+
+        var getAmv = function (id) {
+            return self.AmvResource.query({amvId: id}).$promise;
+        };
+
+        var createAmv = function (params, success, error) {
+            new self.AmvResource(params).$save(success, error);
+        };
+
+        var updateAmv = function (params, success, error) {
+            self.AmvResource.update(params, success, error);
+        };
+
+        var deleteAmv = function (id, success, error) {
+            self.AmvResource.delete({amvId: id}, success, error);
+        };
+
+        var deleteMassAmv = function (ids, success, error) {
+            MassDeleteService.deleteMass('/api/amvs', ids, success, error);
+        };
+
+        var patchAmv = function (id, params, success, error) {
+            self.AmvResource.patch({amvId: id}, params, success, error);
+        }
+
+        return {
+            getAmvs: getAmvs,
+            getAmv: getAmv,
+            createAmv: createAmv,
+            deleteAmv: deleteAmv,
+            deleteMassAmv: deleteMassAmv,
+            updateAmv: updateAmv,
+            patchAmv: patchAmv
+        };
+    }
+
+})
+();
---
+++ src/CategoryService.js
@@ -0,0 +1,50 @@
+(function () {
+
+    angular
+        .module('BackofficeApp')
+        .factory('CategoryService', [ '$resource', CategoryService ]);
+
+    function CategoryService($resource) {
+        var self = this;
+
+        self.CategoryResource = $resource('/api/rolf-categories/:categoryId', { categoryId: '@id' },
+            {
+                'update': {
+                    method: 'PUT'
+                },
+                'query': {
+                    isArray: false
+                }
+            });
+
+        var getCategories = function (params) {
+            return self.CategoryResource.query(params).$promise;
+        };
+
+        var getCategory = function (id) {
+            return self.CategoryResource.query({categoryId: id}).$promise;
+        };
+
+        var createCategory = function (params, success, error) {
+            new self.CategoryResource(params).$save(success, error);
+        };
+
+        var updateCategory = function (params, success, error) {
+            self.CategoryResource.update(params, success, error);
+        };
+
+        var deleteCategory = function (id, success, error) {
+            self.CategoryResource.delete({categoryId: id}, success, error);
+        };
+
+        return {
+            getCategories: getCategories,
+            getCategory: getCategory,
+            createCategory: createCategory,
+            deleteCategory: deleteCategory,
+            updateCategory: updateCategory
+        };
+    }
+
+})
+();
