dfb0ed4d54e67eca0be83375bdb9381ac80b58c4
Cyril Rouyer <cyril@netlor.fr>
2016-10-31 14:07:27 +0100

    Adding edit-field class ton inputs generated by the editable directive

--- src/EditableDirective.js
+++ src/EditableDirective.js
@@ -83,9 +83,9 @@ angular.module('AnrModule').directive('editable', function(){
 		require: ['^^editable', '^^editModel'],
 		restrict: 'A',
 		template: '<span ng-if="! field.edited">{{field.model[field.name]}}</span>\
-							<input ng-class="{editerror: field.error}" ng-if="field.edited && field.type == \'text\'" type="text" ng-model="field.model[field.name]"  escape="cancelEdition()"  action="saveEdition" autofocus/>\
-							<input ng-class="{editerror: field.error}" ng-if="field.edited && field.type == \'number\'" type="number" ng-model="field.model[field.name]"  escape="cancelEdition()" action="saveEdition" autofocus/>\
-							<textarea ng-class="{editerror: field.error}" ng-if="field.edited && field.type == \'textarea\'" ng-model="field.model[field.name]" escape="cancelEdition()" action="saveEdition" autofocus></textarea>',
+							<input class="edit-field" ng-class="{editerror: field.error}" ng-if="field.edited && field.type == \'text\'" type="text" ng-model="field.model[field.name]"  escape="cancelEdition()"  action="saveEdition" autofocus/>\
+							<input class="edit-field" ng-class="{editerror: field.error}" ng-if="field.edited && field.type == \'number\'" type="number" ng-model="field.model[field.name]"  escape="cancelEdition()" action="saveEdition" autofocus/>\
+							<textarea class="edit-field" ng-class="{editerror: field.error}" ng-if="field.edited && field.type == \'textarea\'" ng-model="field.model[field.name]" escape="cancelEdition()" action="saveEdition" autofocus></textarea>',
 		scope: {
 			name: '@editField'
 		},
