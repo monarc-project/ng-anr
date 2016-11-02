5ae57855b2515f5b1fb252dbde45f4bab2732e94
Guillaume Lesniak <guillaume@netlor.fr>
2016-11-02 17:08:07 +0100

    Remove debug conole log

--- src/EditableDirective.js
+++ src/EditableDirective.js
@@ -103,7 +103,6 @@ angular.module('AnrModule').directive('editable', function(){
 				type: attrs.editType && attrs.editType != "" ? attrs.editType : 'text',
 				editedValue: null,
 				edit: function(){
-					console.log(scope.localmodel);
 					this.edited = true;
 					this.initialValue = this.model[this.name];
 					this.editedValue = angular.copy(this.model[this.name]);
