5d2b3d5c6f9f80c087aa6bc9f2e45a468961b731
Guillaume Lesniak <guillaume@netlor.fr>
2016-12-09 16:14:33 +0100

    ANR: Risk sheet: FO: Add recommandations

--- src/AnrLayoutCtrl.js
+++ src/AnrLayoutCtrl.js
@@ -266,6 +266,10 @@
                 }
             }
             $scope.reducAmount = reducAmount;
+
+            if ($scope.OFFICE_MODE == 'FO') {
+                updateRecommandations();
+            }
         };
 
         $scope.resetSheet = function () {
@@ -297,6 +301,98 @@
             })
         };
 
+        $scope.queryRecSearch = function (query) {
+            var RecommandationService = $injector.get('ClientRecommandationService');
+            var q = $q.defer();
+            RecommandationService.getRecommandations({anr: $scope.model.anr.id, filter: query}).then(function (data) {
+                q.resolve(data.recommandations);
+            }, function () {
+                q.reject();
+            });
+
+            return q.promise;
+        };
+
+        $scope.rec_edit = {
+            rec: null
+        };
+        
+        $scope.attachRecommandation = function () {
+            var RecommandationService = $injector.get('ClientRecommandationService');
+            RecommandationService.attachToRisk($scope.model.anr.id, $scope.rec_edit.rec.id, $scope.sheet_risk.id, false,
+                function () {
+                    toastr.success(gettextCatalog.getString("The recommandation has been attached to this risk."));
+                    updateRecommandations();
+                });
+        };
+
+        $scope.detachRecommandation = function (ev, recommandation) {
+            var confirm = $mdDialog.confirm()
+                .title(gettextCatalog.getString('Are you sure you want to detach recommendation "{{ code }}"?',
+                    {code: recommandation.recommandation.code}))
+                .textContent(gettextCatalog.getString('This operation is irreversible.'))
+                .targetEvent(ev)
+                .ok(gettextCatalog.getString('Detach'))
+                .cancel(gettextCatalog.getString('Cancel'));
+            $mdDialog.show(confirm).then(function() {
+                var RecommandationService = $injector.get('ClientRecommandationService');
+                RecommandationService.detachFromRisk($scope.model.anr.id, recommandation.id,
+                    function () {
+                        updateRecommandations();
+                        toastr.success(gettextCatalog.getString('The recommendation has been detached.'),
+                            gettextCatalog.getString('Operation successful'));
+                    }
+                );
+            });
+        }
+
+        $scope.attachMeasureToRecommandation = function (ev, recommandation) {
+            var useFullScreen = ($mdMedia('sm') || $mdMedia('xs'));
+
+            $mdDialog.show({
+                controller: ['$scope', '$mdDialog', 'MeasureService', MeasureRecommandationAttachDialog],
+                templateUrl: '/views/anr/create.recommandation-measure.html',
+                preserveScope: false,
+                targetEvent: ev,
+                scope: $scope.$dialogScope.$new(),
+                clickOutsideToClose: false,
+                fullscreen: useFullScreen,
+            }).then(function (measure_id) {
+                var RecommandationService = $injector.get('ClientRecommandationService');
+                RecommandationService.attachMeasureToRecommandation($scope.model.anr.id, recommandation.id, measure_id, function () {
+                    toastr.success(gettextCatalog.getString("Measure attached to recommendation"));
+                    updateRecommandations();
+                });
+            });
+        };
+
+        $scope.detachMeasureFromRecommandation = function (ev, recommandation, measure) {
+            var confirm = $mdDialog.confirm()
+                .title(gettextCatalog.getString('Are you sure you want to detach measure {{measure}} recommendation "{{ code }}"?',
+                    {measure: measure.code, code: recommandation.recommandation.code}))
+                .textContent(gettextCatalog.getString('This operation is irreversible.'))
+                .targetEvent(ev)
+                .ok(gettextCatalog.getString('Detach'))
+                .cancel(gettextCatalog.getString('Cancel'));
+            $mdDialog.show(confirm).then(function() {
+                var RecommandationService = $injector.get('ClientRecommandationService');
+                RecommandationService.detachMeasureFromRecommandation($scope.model.anr.id, recommandation.measure.id,
+                    function () {
+                        updateRecommandations();
+                        toastr.success(gettextCatalog.getString('The measure has been detached.'),
+                            gettextCatalog.getString('Operation successful'));
+                    }
+                );
+            });
+        }
+
+
+        var updateRecommandations = function () {
+            var RecommandationService = $injector.get('ClientRecommandationService');
+            RecommandationService.getRiskRecommandations($scope.model.anr.id, $scope.sheet_risk.id, false).then(function (data) {
+                $scope.recommandations = data['recommandations-risks'];
+            })
+        };
 
         /**
          * Risk analysis
@@ -1260,6 +1356,50 @@
                 fullscreen: useFullScreen
             });
         }
+
+        $scope.createRecommandation = function (ev) {
+            var useFullScreen = ($mdMedia('sm') || $mdMedia('xs'));
+
+            $mdDialog.show({
+                controller: ['$scope', '$mdDialog', CreateRecommandationDialog],
+                templateUrl: '/views/anr/create.recommandation.html',
+                targetEvent: ev,
+                preserveScope: false,
+                scope: $scope.$dialogScope.$new(),
+                clickOutsideToClose: false,
+                fullscreen: useFullScreen
+            }).then(function (rec) {
+                var RecommandationService = $injector.get("ClientRecommandationService");
+                rec.anr = $scope.model.anr.id;
+                RecommandationService.createRecommandation(rec, function () {
+                    toastr.success(gettextCatalog.getString("The recommendation has been created successfully"));
+                })
+            });
+        }
+
+        $scope.editRecommandation = function (ev, rec) {
+            var useFullScreen = ($mdMedia('sm') || $mdMedia('xs'));
+
+            $mdDialog.show({
+                controller: ['$scope', '$mdDialog', 'rec', CreateRecommandationDialog],
+                templateUrl: '/views/anr/create.recommandation.html',
+                targetEvent: ev,
+                preserveScope: false,
+                scope: $scope.$dialogScope.$new(),
+                clickOutsideToClose: false,
+                fullscreen: useFullScreen,
+                locals: {
+                    rec: angular.copy(rec.recommandation)
+                }
+            }).then(function (rec) {
+                var RecommandationService = $injector.get("ClientRecommandationService");
+                rec.anr = $scope.model.anr.id;
+                RecommandationService.updateRecommandation(rec, function () {
+                    toastr.success(gettextCatalog.getString("The recommendation has been updated successfully"));
+                    updateRecommandations();
+                })
+            });
+        }
     }
 
     // Dialogs
@@ -1660,5 +1800,35 @@
             $mdDialog.cancel();
         };
     }
+
+    function CreateRecommandationDialog($scope, $mdDialog, rec) {
+        $scope.recommandation = rec;
+
+        $scope.create = function () {
+            $mdDialog.hide($scope.recommandation);
+        }
+
+        $scope.cancel = function() {
+            $mdDialog.cancel();
+        };
+    }
+
+    function MeasureRecommandationAttachDialog($scope, $mdDialog, MeasureService) {
+        MeasureService.getMeasures({limit: 0}).then(function (data) {
+            $scope.measures = data.measures;
+        });
+
+        $scope.rec = {
+            measure: 0
+        };
+
+        $scope.create = function () {
+            $mdDialog.hide($scope.rec.measure);
+        };
+
+        $scope.cancel = function () {
+            $mdDialog.cancel();
+        }
+    };
 
 })();
