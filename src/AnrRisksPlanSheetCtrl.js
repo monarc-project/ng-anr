23ee42758a566bbdabf97eafe89f8e332f24fa65
Guillaume Lesniak <guillaume@netlor.fr>
2016-12-13 16:40:18 +0100

    Risksplan: Fix return button

--- src/AnrRisksPlanSheetCtrl.js
+++ src/AnrRisksPlanSheetCtrl.js
@@ -14,6 +14,10 @@
     function AnrRisksPlanSheetCtrl($scope, toastr, $mdMedia, $mdDialog, $stateParams, gettextCatalog, $state,
                                    TreatmentPlanService, ClientRecommandationService, $q) {
 
+        $scope.backToList = function () {
+            $state.transitionTo('main.project.anr.risksplan', {modelId: $stateParams.modelId});
+        };
+
     }
 
 })();
