a0002e24d031419f9b4bfba32d2a8efe4f2a20a1
Guillaume Lesniak <guillaume@netlor.fr>
2016-12-13 16:39:18 +0100

    Add risks plan sheet

---
+++ src/AnrRisksPlanSheetCtrl.js
@@ -0,0 +1,19 @@
+(function () {
+
+    angular
+        .module('AnrModule')
+        .controller('AnrRisksPlanSheetCtrl', [
+            '$scope', 'toastr', '$mdMedia', '$mdDialog', '$stateParams', 'gettextCatalog', '$state', 'TreatmentPlanService',
+            'ClientRecommandationService', '$q',
+            AnrRisksPlanSheetCtrl
+        ]);
+
+    /**
+     * ANR > RISKS PLAN PROCESSING
+     */
+    function AnrRisksPlanSheetCtrl($scope, toastr, $mdMedia, $mdDialog, $stateParams, gettextCatalog, $state,
+                                   TreatmentPlanService, ClientRecommandationService, $q) {
+
+    }
+
+})();
