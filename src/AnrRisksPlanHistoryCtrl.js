d5937dfea82e56bf6625f5638d1f0524a6f2bd3c
Guillaume Lesniak <guillaume@netlor.fr>
2016-12-14 09:21:12 +0100

    Risks plan: History: Create entry dialog

--- src/AnrRisksPlanHistoryCtrl.js
+++ src/AnrRisksPlanHistoryCtrl.js
@@ -18,6 +18,35 @@
             $state.transitionTo('main.project.anr.risksplan', {modelId: $stateParams.modelId});
         };
 
+        $scope.createHistoryEntry = function (ev) {
+            var useFullScreen = ($mdMedia('sm') || $mdMedia('xs'));
+
+            $mdDialog.show({
+                controller: ['$scope', '$mdDialog', CreateHistoryEntryDialog],
+                templateUrl: '/views/anr/create.historyentry.html',
+                targetEvent: ev,
+                preserveScope: false,
+                scope: $scope.$dialogScope.$new(),
+                clickOutsideToClose: false,
+                fullscreen: useFullScreen,
+            }).then(function (exports) {
+
+            });
+        };
+
+    }
+
+
+    function CreateHistoryEntryDialog($scope, $mdDialog) {
+        $scope.etnry = {};
+
+        $scope.cancel = function () {
+            $mdDialog.cancel();
+        }
+
+        $scope.create = function () {
+            $mdDialog.hide($scope.entry);
+        }
     }
 
 })();
