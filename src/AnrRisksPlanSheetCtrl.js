fe57f0f005bf3b5012b5719c0057d29922230efb
Guillaume Lesniak <guillaume@netlor.fr>
2016-12-15 09:51:31 +0100

    Add risk sheet validation dialog impl

--- src/AnrRisksPlanSheetCtrl.js
+++ src/AnrRisksPlanSheetCtrl.js
@@ -18,6 +18,37 @@
             $state.transitionTo('main.project.anr.risksplan', {modelId: $stateParams.modelId});
         };
 
+        $scope.validate = function (ev, measure) {
+            var useFullScreen = ($mdMedia('sm') || $mdMedia('xs'));
+
+            $mdDialog.show({
+                controller: ['$scope', '$mdDialog', 'recommendation', 'measure', ValidateMeasureDialog],
+                templateUrl: '/views/anr/validate.recommandation.html',
+                targetEvent: ev,
+                preserveScope: false,
+                scope: $scope.$dialogScope.$new(),
+                clickOutsideToClose: false,
+                fullscreen: useFullScreen,
+                locals: {
+                    recommendation: $scope.rec,
+                    measure: measure
+                }
+            }).then(function (exports) {
+
+            });
+        };
+
+    }
+
+
+    function ValidateMeasureDialog($scope, $mdDialog, recommendation, measure) {
+        $scope.create = function () {
+            $mdDialog.hide($scope.valid);
+        };
+
+        $scope.cancel = function () {
+            $mdDialog.cancel();
+        }
     }
 
 })();
