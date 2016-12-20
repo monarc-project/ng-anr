e18e92c27ba298961fdb1ad5f0dac1227d3da451
Guillaume Lesniak <guillaume@netlor.fr>
2016-12-20 14:16:36 +0100

    ANR: Update ANR information after editing

--- src/AnrLayoutCtrl.js
+++ src/AnrLayoutCtrl.js
@@ -1060,6 +1060,7 @@
                             $scope.model.showRolfBrut = anr.showRolfBrut;
                         }
                     });
+                    $scope.model.anr = anr;
                 });
         };
 
