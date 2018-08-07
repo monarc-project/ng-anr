818b58852e534ba3f0b998ce79e9f338ef001c60
Jerome Lombardi <jerome.lombardi@cases.lu>
2018-08-07 09:03:08 +0200

    remove console.log

--- src/AnrLayoutCtrl.js
+++ src/AnrLayoutCtrl.js
@@ -1124,7 +1124,6 @@
                 }
 
                 var recurseFillTree = function (category, depth) {
-                  console.log('dede');
                     var output = {id: category.id, type: 'libcat', label1: category.label1, label2: category.label2,
                         label3: category.label3, label4: category.label4, depth: depth, __children__: []};
 
@@ -1175,7 +1174,6 @@
                 $scope.first_object = null;
                 $scope.has_virtual_categ = false;
                 for (var v = 0; v < data.categories.length; ++v) {
-                  console.log(data.categories[v]);
                     if(data.categories[v].child || data.categories[v].objects.length > 0){ //do not show empty category
                       var cat = data.categories[v];
                       if(cat.id == -1){
