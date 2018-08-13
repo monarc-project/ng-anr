1fc88a82b2a0267a8e1e5496d70514d15cc60386
Jerome Lombardi <jerome.lombardi@cases.lu>
2018-08-13 13:23:51 +0200

    put the management of SOA category available for BO too

---
+++ src/ClientCategoryService.js
@@ -0,0 +1,88 @@
+(function () {
+
+    angular
+        .module('AnrModule')
+        .factory('ClientCategoryService', [ '$resource', '$rootScope', 'MassDeleteService', ClientCategoryService ]);
+
+    function ClientCategoryService($resource, $rootScope, MassDeleteService) {
+        var self = this;
+
+
+
+        var makeResource = function () {
+           var anr =  $rootScope.OFFICE_MODE == "FO" ? "client-anr/:urlAnrId/" : "";
+
+            self.ClientCategoryResource = $resource('api/' + anr + 'category/', {
+                    categoryId: '@id',
+                    urlAnrId: $rootScope.getUrlAnrId()
+                },
+                {
+                    'update': {
+                        method: 'PUT'
+                    },
+                    'patch': {
+                        method: 'PATCH'
+                    },
+                    'query': {
+                        isArray: false
+                    }
+                });
+        }
+        makeResource();
+
+
+
+                var getCategories = function (params) {
+                    makeResource();
+                    return self.ClientCategoryResource.query(params).$promise;
+                };
+
+                var getCategory = function (params) {
+                    makeResource();
+                    return self.ClientCategoryResource.query({'anr': params.anr, 'id': params.id}).$promise;
+                };
+
+                var createCategory = function (params, success, error) {
+                    new self.ClientCategoryResource(params).$save(success, error);
+                };
+
+                var updateCategory = function (params, success, error) {
+                    self.ClientCategoryResource.update(params, success, error);
+                };
+
+                var deleteCategory = function (id, success, error) {
+                    self.ClientCategoryResource.delete({categoryId: id}, success, error);
+                };
+
+
+                var deleteMassCategory = function (ids, success, error) {
+
+                    if ($rootScope.OFFICE_MODE == 'FO') {
+                        MassDeleteService.deleteMass('api/client-anr/' + $rootScope.getUrlAnrId() + '/category', ids, success, error);
+                    } else {
+                        MassDeleteService.deleteMass('api/category', ids, success, error);
+                    }
+                }
+
+                var patchCategory = function (id, params, success, error) {
+
+                    self.ClientCategoryResource.patch({categoryId: id}, params, success, error);
+                }
+
+                return {
+                    getCategories: getCategories,
+                    getCategory: getCategory,
+                    createCategory: createCategory,
+                    deleteCategory: deleteCategory,
+                    updateCategory: updateCategory,
+                    deleteMassCategory:deleteMassCategory,
+                    patchCategory:patchCategory
+
+                };
+
+
+
+    }
+
+})
+();
