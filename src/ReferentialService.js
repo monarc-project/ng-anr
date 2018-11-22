3423e80b8fa47c52528402a447aaca40ac4bbff0
Juan Rocha <juan.rocha@securitymadein.lu>
2018-11-22 13:12:02 +0100

    Typo

--- src/ReferentialService.js
+++ src/ReferentialService.js
@@ -41,7 +41,6 @@
         };
 
         var updateReferential = function (params, success, error) {
-            console.log('update referential');
             self.ReferentialResource.update(params, success, error);
         };
 
