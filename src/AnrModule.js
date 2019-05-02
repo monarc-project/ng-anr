97b024659a06d190c8d2a956f01320178d1ca47e
Juan Rocha <juan.rocha@securitymadein.lu>
2019-05-02 16:17:21 +0200

    Add angular-loading-bar

--- src/AnrModule.js
+++ src/AnrModule.js
@@ -1,1 +1,5 @@
-angular.module('AnrModule', ['angularResizable']);
+angular.module('AnrModule', ['angularResizable', 'angular-loading-bar', 'ngAnimate'])
+        .config(['cfpLoadingBarProvider', function(cfpLoadingBarProvider) {
+        cfpLoadingBarProvider.latencyThreshold = 1000;
+        cfpLoadingBarProvider.includeSpinner = false;
+        }]);
